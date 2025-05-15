75088cb949b1118b4860c7b340a392053176dcb0
Suman-CLoud2024 <suman.anantha@gmail.com>
2025-05-15 13:31:58 +0530

    second msg

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return jsonify(message="Running through Ngork!")
+    return jsonify(message="Running through Nork!")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)

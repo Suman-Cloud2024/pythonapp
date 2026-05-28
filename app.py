940cc2136949c09b143e7238c75a6bfb886e7fc5
Suman-CLoud2024 <suman.anantha@gmail.com>
2026-05-28 22:54:48 +0530

    first message

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify(message="This is Suman Anantha - Welcome to my Flask application!")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
